0e2c3ccb90c66630be76187ce0d624cb4fd79136
= <=>
2024-08-16 06:30:40 -0300

    Funciona estadisticas, jugadores y server

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, jsonify
 from flask_mysqldb import MySQL
 import os
@@ -8,7 +9,7 @@ app = Flask(__name__)
 # Configuración de la base de datos MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '123456789'
 app.config['MYSQL_DB'] = 'copa_america'
 
 mysql = MySQL(app)
@@ -33,24 +34,32 @@ def jugadores():
     jugador_id = request.form.get('player_id')
 
     if seleccion:
-        # Obtener los jugadores del país seleccionado
         cursor.execute("SELECT id, nombre, apellido FROM jugadores WHERE nacionalidad = %s", (seleccion,))
         jugadores = cursor.fetchall()
 
     if jugador_id:
-        # Obtener toda la información del jugador seleccionado
         cursor.execute("""
             SELECT nombre, apellido, edad, equipo_actual, historial_equipos, 
-                   tarjetas_rojas, tarjetas_amarillas, imagen, altura, peso, 
-                   goles_totales, asistencias_totales, partidos_totales, 
-                   lesiones_recientes, redes_sociales, pie_dominante, fecha_nacimiento 
+                   tarjetas_rojas, tarjetas_amarillas, imagen 
             FROM jugadores WHERE id = %s
         """, (jugador_id,))
         jugador_info = cursor.fetchone()
 
     cursor.close()
     return render_template('jugadores.html', selecciones=selecciones, jugadores=jugadores, jugador_info=jugador_info, seleccion=seleccion, jugador_id=jugador_id)
-   
+
+# Ruta para obtener jugadores de una selección específica
+@app.route('/jugadores/seleccion', methods=['GET'])
+def obtener_jugadores_por_seleccion():
+    seleccion = request.args.get('seleccion')
+    cursor = mysql.connection.cursor()
+
+    cursor.execute("SELECT nombre, apellido FROM jugadores WHERE nacionalidad = %s", (seleccion,))
+    jugadores = [{"nombre": f"{fila[0]} {fila[1]}"} for fila in cursor.fetchall()]
+    cursor.close()
+
+    return jsonify(jugadores)
+
 # Ruta para obtener y mostrar estadísticas de un jugador específico
 @app.route('/estadisticas', methods=['GET'])
 def estadisticas():
